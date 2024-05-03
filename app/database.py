27ef4485bfed69f20153c2f9695583e7e29f9b71
maximt <vbmaxim@gmail.com>
2024-05-03 13:59:59 +0700

    sqlalchemy connection and models

---
+++ app/database.py
@@ -0,0 +1,35 @@
+import os
+from datetime import datetime
+
+from sqlalchemy import create_engine, Integer, String, DateTime
+from sqlalchemy.sql import func
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, Session
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+class Task(Base):
+    __tablename__ = 'todobot_tasks'
+
+    id = mapped_column(Integer, primary_key=True)
+    text = mapped_column(String(2048), nullable=False)
+
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True), server_default=func.now()
+    )
+
+    def __repr__(self):
+        return f"<Task(id={self.id}: text={self.text})>"
+
+    def __str__(self):
+        return f"{self.id}: {self.text}"
+
+
+engine = create_engine(
+    "postgresql+psycopg2://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}/{POSTGRES_DB}".format(**os.environ)
+)
+Base.metadata.create_all(engine)
+
+session = Session(engine)
