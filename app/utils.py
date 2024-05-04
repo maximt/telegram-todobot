3ddf553175dcdd5e656cdd7988c0ffcc84f05638
maximt <vbmaxim@gmail.com>
2024-05-04 20:13:18 +0700

    helpers to parse messages

---
+++ app/utils.py
@@ -0,0 +1,15 @@
+import re
+
+
+def strip_command(msg: str | None) -> str | None:
+    if not msg:
+        return None
+    match = re.search(r"(/\w+\s+)(.*)$", msg)
+    return match.group(2) if match else None
+
+
+def find_index_in_text(msg: str | None) -> str | None:
+    if not msg:
+        return None
+    match = re.search(r"\#(\d+)", msg)
+    return match.group(1) if match else None
