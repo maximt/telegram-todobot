a459d2d511da5daf20f9cafe8b85cb80fee4bbd1
maximt <vbmaxim@gmail.com>
2024-05-05 11:32:27 +0700

    fix types

--- app/utils.py
+++ app/utils.py
@@ -8,8 +8,8 @@ def strip_command(msg: str | None) -> str | None:
     return match.group(2) if match else None
 
 
-def find_index_in_text(msg: str | None) -> str | None:
+def find_index_in_text(msg: str | None) -> int | None:
     if not msg:
         return None
     match = re.search(r"\#(\d+)", msg)
-    return match.group(1) if match else None
+    return int(match.group(1)) if match else None
